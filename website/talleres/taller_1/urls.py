d46c45416dd1aa1adf5e2357c7b61435b457f46d
minigonche <minigonche@gmail.com>
2020-02-10 19:43:52 -0500

    Added landing page

--- website/talleres/taller_1/urls.py
+++ website/talleres/taller_1/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'taller_1'
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('iniciar_sesion', views.iniciar_sesion, name='iniciar_sesion'),
+    path('crear_usuario', views.crear_usuario, name='crear_usuario'),
+    
 ]
--- website/talleres/taller_1/views.py
+++ website/talleres/taller_1/views.py
@@ -4,6 +4,44 @@ from django.http import HttpResponse
 
 
 def index(request):
-    
-    context = {}
-    return render(request, 'taller_1/index.html', context)
+	'''
+	Metodo de bienvenida
+	'''
+
+	context = {}
+	return render(request, 'taller_1/index.html', context)
+
+
+
+
+def iniciar_sesion(request):
+	'''
+	Metodo para crear una nueva sesion. 
+	Debe verificar el ususario y la contrasenha
+
+	TODO: Verificar usuario existente y contrasenha correcta
+	'''
+
+	# Extrae los datos
+	id_ususario = request.POST.get('id_ususario_registrado')
+	pwd = request.POST.get('pwd_ususario_registrado')
+
+	return HttpResponse("Login Usuario \n Id: {}, pwd: {}".format(id_ususario,pwd))
+
+
+def crear_usuario(request):
+	'''
+	Metodo para crear un nuevo usuario.
+
+	Debe verificar que no existe el id del susuario solicitado.
+
+	TODO: Verificar que no existe el ususario actual y crearlo.
+	'''
+
+	# Extrae los datos
+	id_ususario = request.POST.get('id_ususario_nuevo')
+	pwd = request.POST.get('pwd_ususario_nuevo')
+	edad = request.POST.get('edad')
+	sexo = request.POST.get('sexo')
+
+	return HttpResponse("Login Usuario Viejo. Id: {}, pwd: {}, edad: {}, sexo: {}".format(id_ususario,pwd, edad, sexo))
