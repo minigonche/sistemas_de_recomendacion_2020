15d2c0b507745fd4263fc0f90f4506f62565e958
minigonche <minigonche@gmail.com>
2020-02-12 21:28:28 -0500

    Agrega la verificacion de usuarios

--- website/talleres/taller_1/models.py
+++ website/talleres/taller_1/models.py
@@ -1,6 +1,66 @@
 from django.db import models
 
 
+# User
 class User(models.Model):
-    user_id = models.CharField(max_length=20)
-    password = models.CharField(max_length=20)
+
+	# User ID
+	user_id = models.CharField(max_length=20)
+	# Password
+	password = models.CharField(max_length=20)
+	# Date the user joined
+	date_join = models.DateField(null=True)
+	# Age
+	age = models.PositiveIntegerField(null=True)
+	# Sex
+	sex = models.NullBooleanField(null=True)
+	#country
+	country = models.CharField(max_length=25, null=True)
+
+
+	def dar_sexo(self):
+
+		sexo = 'No hay informacion'
+		if self.sex is not None:
+			if self.sex:
+				sexo = 'Hombre'
+			else:
+				sexo = 'Mujer'
+
+		return(sexo)
+
+
+	def dar_pais(self):
+
+		pais = 'No hay informacion'
+		if self.country is not None:
+			pais = self.country
+
+		return(pais)
+
+	def dar_fecha(self):
+
+		fecha_registro = 'No tiene'
+		if self.date_join is not None:
+			fecha_registro = self.date_join.strftime('%Y-%m-%d')
+
+		return(fecha_registro)
+
+
+	def dar_edad(self):
+
+		edad = 'No hay informacion'
+		if self.age > 0:
+			edad = self.age
+
+		return(edad)
+
+
+	def __str__(self):
+
+		usuario = self.user_id
+		pwd = self.password
+
+		return('Usuario: {}, Pwd: {}, Fecha Registro: {}, Edad: {}, Sexo: {}, Pais: {}'.format(usuario, pwd, self.dar_fecha(), self.dar_edad(), self.dar_sexo(), self.dar_pais()))
+
+
--- website/talleres/taller_1/views.py
+++ website/talleres/taller_1/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
+from datetime import datetime
+from taller_1.models import User
 
 def index(request):
 	'''
@@ -26,7 +26,18 @@ def iniciar_sesion(request):
 	id_ususario = request.POST.get('id_ususario_registrado')
 	pwd = request.POST.get('pwd_ususario_registrado')
 
-	return HttpResponse("Login Usuario \n Id: {}, pwd: {}".format(id_ususario,pwd))
+	# Busca el usuario
+	try:
+		user = User.objects.get(user_id=id_ususario)
+
+		# Revisa la contrasenha
+		if user.password == pwd:
+			return(HttpResponse(str(user)))
+		else:
+			return HttpResponse('Contrasenha equivocada')
+
+	except:
+		return HttpResponse('Usuario: {} no existe'.format(id_ususario))
 
 
 def crear_usuario(request):
@@ -41,7 +52,41 @@ def crear_usuario(request):
 	# Extrae los datos
 	id_ususario = request.POST.get('id_ususario_nuevo')
 	pwd = request.POST.get('pwd_ususario_nuevo')
-	edad = request.POST.get('edad')
+	
 	sexo = request.POST.get('sexo')
 
-	return HttpResponse("Login Usuario Viejo. Id: {}, pwd: {}, edad: {}, sexo: {}".format(id_ususario,pwd, edad, sexo))
+	if sexo == 'Hombre':
+		sexo = True
+	elif sexo == 'Mujer':
+		sexo = False
+	else:
+		sexo = None
+
+	edad = request.POST.get('edad')
+	try:
+		edad = int(edad)
+	except:
+		edad = 0
+
+
+	pais = request.POST.get('pais')
+	fecha_registro = datetime.now()
+
+	# Mira si el usuario ya existe
+	try:
+		user = User.objects.get(user_id=id_ususario)
+		return HttpResponse('Usuario: {} ya existe'.format(id_ususario))
+	except:
+
+		user = User(user_id= id_ususario, 
+				    password= pwd,
+				    date_join= fecha_registro,
+				    age= edad,
+				    sex= sexo,
+				    country= pais)
+
+		user.save()
+
+		return HttpResponse(str(user))
+
+
