3cd7a32a9ff0d42aabf75ce282e7b469d3a26817
minigonche <minigonche@gmail.com>
2020-05-03 20:14:19 -0500

    Modelos

--- website/talleres/taller_2/otros/modelo_factorizado.py
+++ website/talleres/taller_2/otros/modelo_factorizado.py
@@ -8,28 +8,37 @@ import pandas as pd
 import numpy as np
 
 
+import gzip
+import surprise
 
 todos_lugares = set(['OTHER', 'NC', 'AZ', 'QC', 'NV', 'IL', 'ON', 'AB', 'PA', 'WI', 'SC', 'OH'])
 
-ubication_datos = ''
 
                 
+ubicacion = 'taller_2/otros/data/SVD/'
 
-def dar_recomendaciones(lugar, usuario, top = 50):
-	'''
-	Metodo que devuelve el numero de lugares solicitado de ids de lugares, ordenados de mejor a peor.
 
-	El metodo devuelve un codigo indicando lo sucedido. Asi son los codigos:
-		(Libre de poner lo que quiera, pero es para usar en la siguiente capa, por si algo. Puede mirar los que yo puse en modelo_ponderado.dar_recomendaciones())
-	Returns
-	--------
-	Devuelve lo siguiente
-		- array string: una lista con los ids de los lugares
-		- array float: una lista con las calificaciones correpsondientes
-		- string: codigo indicando lo que sicedio
-		- string: explicacion
 
-	'''
 
-	# TODO: GIO
-	pass
+def dar_calificacion(id_usuarios, id_businesses):
+    
+    id_usuario = id_usuarios[0]
+    id_business = id_businesses[0]
+    
+    # Lee las bd
+    tabla_u = pd.read_pickle(ubicacion + 'usuarios_id.pkl')
+    tabla_b = pd.read_pickle(ubicacion + 'negocios_id.pkl')
+    
+    # Lee la matriz de calificaciones
+    f = gzip.open(ubicacion +'0.Model_SVM.pklz','rb')
+    Model_SVM = pickle.load(f)
+    
+    pred = []
+    for i in range(len(id_usuarios)):
+        usuario = tabla_u.loc[id_usuarios[i]].new_id
+        neg = tabla_b.loc[id_businesses[i]].new_id
+        
+        p = Model_SVM.predict(usuario, neg).est
+        pred.append(p)
+    
+    return(pred)
--- website/talleres/taller_2/otros/modelo_ponderado.py
+++ website/talleres/taller_2/otros/modelo_ponderado.py
@@ -2,9 +2,12 @@
 
 
 import numpy as np
+import pandas as pd
 import taller_2.otros.modelo_contenido as modelo_contenido 
+import taller_2.otros.modelo_factorizado as modelo_factorizado 
 
 import taller_2.otros.back_end as be
+
 
 
 def dar_recomendaciones(lugar, usuario, top = 50):
@@ -18,16 +21,32 @@ def dar_recomendaciones(lugar, usuario, top = 50):
 	Devuelve lo siguiente
 		- Arreglo de lugares?
 	'''
+	usuario_completo = be.dar_usuario(usuario)
+	alfa = 0.5
 
 	# Recomendaciones por el modelo de contenido
 	ids, calif, _, _ = modelo_contenido.dar_recomendaciones(lugar, usuario, top = 50)
 
+	# Valores Modelo Factorizado
+	calif_svd = modelo_factorizado.dar_calificacion([usuario]*len(ids), ids)
+
+	df_final = pd.daatFrame({'business_id':ids, 'calif_con': calif, 'calif_fac':calif_svd})
+
+	
+	df_final['calif'] = alpha*df_final.calif_con + (1-alpha)*df_final.calif_fac
+	df_final.sort_values('calif', ascending = False, inplace = True)
+	
+
+	df_final = df_final.iloc[0:min(top, df_final.shape[0])]
 
 	negocios = []
-	for i in range(len(ids)):
-		business_id = ids[i]
+	for ind, row in df_final.iterrows():
+		
+		business_id = row.business_id
+
 		neg = be.dar_negocio(business_id)
-		neg.predicted_stars = np.round(calif[i],2)
+
+		neg.predicted_stars = np.round(row.calif,2)
 
 		if neg.categories is None:
 			neg.categoria = 'Ninguna'
